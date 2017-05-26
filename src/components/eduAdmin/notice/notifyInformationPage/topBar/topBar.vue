8d1ec13066bc3cc9986e9c2f2c1574a175162695
lanshanlan <2931735037@qq.com>
2017-05-26 11:05:25 +0800

    2017.5.26 morning

--- src/components/eduAdmin/notice/notifyInformationPage/topBar/topBar.vue
+++ src/components/eduAdmin/notice/notifyInformationPage/topBar/topBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="mngGradePageTopBar" class="topBarDiv am-btn-group">
+  <div id="notifyInfoTopBar" class="topBarDiv am-btn-group">
     <!--导航栏-->
     <router-link to="/eduAdmin/information/noticeManage"><button class="am-btn am-btn-success" style="margin-left: 5rem">公告管理</button></router-link>
     <router-link to="/eduAdmin/information/noticeSetting"><button class="am-btn am-btn-success">公告编辑</button></router-link>
@@ -14,6 +14,24 @@
       return {
         msg: ''
       }
+    },
+    mounted: function(){
+      try{
+        var buttons = document.getElementById("notifyInfoTopBar").getElementsByTagName("button");
+        var authorityList = Json.parse(sessionStorage.getItem("authorityList"));
+        for(var i=0;i<buttons.length;i++){
+          buttons[i].style.display="none";
+        }
+        for(var j=0;j<authorityList.length;i++){
+          if(authorityList[j] == "19"){
+            buttons[0].style.display="inline";
+          }else if(authorityList[j] == "18"){
+            buttons[0].style.display="inline";
+          }else if(authorityList[j] == "57"){
+            buttons[0].style.display="inline";
+          }
+        }
+      }catch(e){}
     }
   }
 </script>
